d65af4ea4a30aeb428c531b75865b30126aae26e
jjwedemyer <mail@jakob-wedemyer.de>
2016-04-20 18:34:58 +0200

    fade() doesn't work rihgt now

--- assets/js/own.js
+++ assets/js/own.js
@@ -22,37 +22,31 @@ function getMobileOperatingSystem() {
   }
 }
 
-/*function snapper(getMobileOperatingSystem()) {
+/* switches avatar effects based on OS.
+ *  Has no return value.
+ */
+function snapper(getMobileOperatingSystem()) {
   var ele = document.getElementById('avatar_span')
-  if (getMobileOperatingSystem == 'iOS' ^ getMobileOperatingSystem == 'Android') {
-    ele.innerHTML = '<a href="snapchat://add/jj_wedemeyer" alt="me on snapchat"><img src="images/avatar.jpg" alt="" height="122" width="122" id="img" /></a>'
-  }
-  if (getMobileOperatingSystem == 'unknown') {
-    ele.innerHTML = '<img src="images/snapcode.png" alt="" height="122" width="122" id="snapcode" />'
+  if (getMobileOperatingSystem == 'iOS' || 'Android') {
+    ele.innerHTML = '<a href="snapchat://add/jj_wedemeyer" alt="me on snapchat"><img src="images/avatar.jpg" alt="" id="img" /></a>'
   }
 }
 
+function fade(id) {
+  document.getElementById(id).classList.toggle('notvisible');
+}
 
-/*cheet.js section*/
-function dancer(dancevid) {
-  if (dancevid == undefined) {
-    document.getElementById('bgvid').style.display = (document.getElementById('bgvid').style.display == 'block' ? 'none' : 'block');
-  }
-  if (dancevid == true) {
-    document.getElementById('bgvid').style.display = 'block';
-  } else if (dancevid == false) {
-    document.getElementById('bgvid').style.display = 'none';
-  }
-};
-/*function pulse() {
-  var children = document.getElementById('pulser').children;
-  for (var i = 0; i < children.length; i++) {
-    children[i].firstElementChild.classList.add('fa-pulse');
-  }
-  document.
-  setTimeout(function() {
-    for (var i = 0; i < children.length; i++) {
-      children[i].firstElementChild.classList.remove('fa-pulse');
+/*
+ * Will switch out links to launch apps if functional
+ *
+ */
+function mobileLinks(getMobileOperatingSystem()) {
+  var elements = document.getElementsByClassName('link')
+  var services = [github,twitter,facebook,snapchat,spotify,youtube,vimeo,vine,soundcloud,twitch]
+
+  if (getMobileOperatingSystem() == 'iOS' || 'Android') {
+    for (ele of elements) {
+
     }
-  }, 5000);
-}*/
+  }
+}
