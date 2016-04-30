76540b5c7f647ea07a2bea08cdab4b4589195568
jjwedemyer <mail@jakob-wedemyer.de>
2016-04-30 19:37:20 +0200

    added FB login, extended user.class , added php features to index

--- assets/php/user.php
+++ assets/php/user.php
@@ -1,15 +1,17 @@
 <?php
+  require 'db_conf.inc.php';
   /**
    * User Object class
    * holds data related to user
    */
   class User
   {
-    public function __construct($id, $name, $fblog, $twlog) //constructor used for creating object
+    public function __construct($id, $name, $mail) //constructor used for creating object
     {
-      $u_name = $name;
-      $handle = creHandle($name);
-      $u_mail = setMail();
+      $this->u_name = $name;
+      $this->handle = creHandle($name);
+      $this->u_mail = setMail($mail);
+      $this->uuid   = $id;
     }
 
     function creHandle($value) // creates user handle based on RealName.
@@ -18,22 +20,56 @@
       $handle = str_replace(' ', '_', $handle);
       return $handle;
     }
-    function setFB($value='')
+    function setMail($mail)
     {
-      global $fbuname = $value;
-      if(global $uuid == )
+      if (filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+        return $mail;
+      }
+      else {
+        return "error";
+      }
     }
+    function setFB($username)
+    {
+
+    }
+
+    public function writeDB()
+    {
+      $con  = mysql_connect($db_host, $db_user, $db_pass);
+      if(! $conn ) {
+        die('Could not connect: ' . mysql_error());
+      }
+      $date = time();
+      $sql  = "INSERT INTO user".
+              "(UUID,realname,handle,mail,bg,fbuname,fbuuid,insta,snap,sncode,twitterhandle,peris,meerkatid,tumblr,join_date)".
+              "VALUES('$this->uuid','$this->displayName','$this->handle','$this->mail','$this->bg','$this->fbuname','$this->fbuuid','$this->insta','$this->scuname','$this->sccode','$this->twhandle','$this->perid','$this->meerkatid','$this->tumbuname',NOW())";
+      mysql_select_db('rage');
+      $retval = mysql_query( $sql, $conn );
+      if(! $retval ) {
+        die('Could not enter data: ' . mysql_error());
+      }
+
+      echo "Entered data successfully\n";
+
+      mysql_close($conn);
+    }
+
     public $uuid        =; //if created with fb == fbuuid otherwise dunno
     public $u_mail      =; //User email
     public $displayName =; // Real Name
     public $handle      =; // username...
+    public $bg          =; //Background pciture
     public $fbuname     =; //Facebook user name
     public $fbuuid      =; //if not in uuid then save here
+    public $insta       =; // Instagram handle
     public $scuname     =; //snapchat user name
-    public $twuname     =; //twitter handle
+    public $sccode      =; //snapcope
+    public $twhandle     =; //twitter handle
     public $tumbuname   =; //tumblr
     public $perid       =; // periscope
     public $meerkatid   =; //meerkat
+    public $tagline     =;
 
   }
 
