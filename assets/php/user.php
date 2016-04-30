23d2d76e57deda620d7dc31a6ff20d2454ff0147
jjwedemyer <mail@jakob-wedemyer.de>
2016-04-30 13:56:58 +0200

    added email/meerkat var

--- assets/php/user.php
+++ assets/php/user.php
@@ -5,13 +5,14 @@
    */
   class User
   {
-    public function __construct($id, $name)
+    public function __construct($id, $name, $fblog, $twlog) //constructor used for creating object
     {
       $u_name = $name;
       $handle = creHandle($name);
+      $u_mail = setMail();
     }
 
-    function creHandle($value)
+    function creHandle($value) // creates user handle based on RealName.
     {
       $handle = strtolower($value);
       $handle = str_replace(' ', '_', $handle);
@@ -22,15 +23,17 @@
       global $fbuname = $value;
       if(global $uuid == )
     }
-    public $uuid      =; //if created with fb == fbuuid otherwise dunno
-    public $u_name    =; // Real Name
-    public $handle    =; // username...
-    public $fbuname   =; //Facebook user name
-    public $fbuuid    =; //if not in uuid then save here
-    public $scuname   =; //snapchat user name
-    public $twuname   =; //twitter handle
-    public $tumbuname =; //tumblr
-    public $perid     =; // periscope
+    public $uuid        =; //if created with fb == fbuuid otherwise dunno
+    public $u_mail      =; //User email
+    public $displayName =; // Real Name
+    public $handle      =; // username...
+    public $fbuname     =; //Facebook user name
+    public $fbuuid      =; //if not in uuid then save here
+    public $scuname     =; //snapchat user name
+    public $twuname     =; //twitter handle
+    public $tumbuname   =; //tumblr
+    public $perid       =; // periscope
+    public $meerkatid   =; //meerkat
 
   }
 
